e23e66bc651d63a9b753da1cbd0ef9cc68caebf3
justianromuel <jastinromuel@gmail.com>
2022-07-13 21:40:41 +0700

    update

--- 3.js
+++ 3.js
@@ -1,12 +1,23 @@
-const arr = [2, 24, 32, 22, 31, 100, 56, 21, 99, 7, 5, 37, 97, 25, 13, 11]
+const arr = [2, 24, 32, 22, 31, 100, 56, 21, 99, 7, 5, 37, 97, 25, 13, 11];
+let ganjil = [];
+let genap = [];
 
-function bubbleSort(arr, n) {
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+        genap.push(arr[i])
+    } else {
+        ganjil.push(arr[i])
+
+    }
+}
+
+function bubbleSort(arr) {
     // cek lenght array
-    if (n == 1)
+    if (arr.length == 1)
         return;
 
     let count = 0;
-    for (let i = 0; i < n - 1; i++)
+    for (let i = 0; i < arr.length - 1; i++)
         if (arr[i] > arr[i + 1]) {
             // swap arr[i], arr[i+1]
             let temp = arr[i];
@@ -18,11 +29,15 @@ function bubbleSort(arr, n) {
     // cek recursion
     // jika recursion tidak ada / tidak terjadi maka di return
     if (count == 0)
-        return console.log("Array:", arr.toString());;
+        return console.log(
+            "Array:", arr.toString(),
+            "\nganjil:", ganjil.toString(),
+            "\ngenap:", genap.toString()
+        );
 
     // nilai paling besar sudah fix
     // recur sisa array yang ada
-    bubbleSort(arr, n - 1);
+    bubbleSort(arr);
 }
 
-bubbleSort(arr, arr.length);
+bubbleSort(arr);
